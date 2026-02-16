20b0869dbbd3232cc1040bbc2802e673a3e1404d
Nalishaa <nalishashakya@gmail.com>
2026-02-16 19:20:22 +0545

    Updating App.jsx

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Items from "./components/Items";
+import { groceryItems } from "./data/groceryItems";
 import './App.css'
 
 function App() {
@@ -32,4 +34,12 @@ function App() {
   )
 }
 
-export default App
+const App = () => {
+  return (
+    <section className="section-center">
+      <Items items={groceryItems} />
+    </section>
+  );
+};
+
+export default App;
